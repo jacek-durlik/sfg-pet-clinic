d7540b04c5e9950d54d39040ec7511dc10ade339
Jacek Durlik <jacek.jan.durlik@gmail.com>
2020-12-20 21:15:17 +0100

    initial project

---
+++ src/test/java/pl/anril/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.anril.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
