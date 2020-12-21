326b29034656765786aeed474f952650e0f72430
Jacek Durlik <jacek.jan.durlik@gmail.com>
2020-12-21 21:47:31 +0100

    POJOs closes #2

--- src/main/java/pl/anril/sfgpetclinic/model/Vet.java
+++
@@ -1,5 +0,0 @@
-package pl.anril.sfgpetclinic.model;
-
-public class Vet extends Person {
-
-}
---
+++ src/main/java/pl/anril/sfgpetclinic/model/Veterinarian.java
@@ -0,0 +1,5 @@
+package pl.anril.sfgpetclinic.model;
+
+public class Veterinarian extends Person {
+
+}
