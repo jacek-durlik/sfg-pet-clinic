6b51e4d20c793e8b10c0f8195cc7901c3f8a90b5
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-01-02 09:11:28 +0100

    listing all vets, closes #14

--- pet-clinic-web/src/main/java/pl/anril/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/anril/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,18 @@
 package pl.anril.sfgpetclinic.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.anril.sfgpetclinic.services.VetService;
 
 @Controller
+@RequiredArgsConstructor
 public class VetController {
+    private final VetService vetService;
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
