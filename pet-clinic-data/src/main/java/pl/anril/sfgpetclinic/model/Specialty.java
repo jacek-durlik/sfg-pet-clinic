d828d780737d824d1998bc472afe6ee69da8d16d
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-01-10 21:50:06 +0100

    add vet specialty, closes #26

---
+++ pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,8 @@
+package pl.anril.sfgpetclinic.model;
+
+import lombok.Data;
+
+@Data
+public class Specialty extends BaseEntity {
+    private String description;
+}
--- pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Vet.java
@@ -1,5 +1,11 @@
 package pl.anril.sfgpetclinic.model;
 
-public class Vet extends Person {
+import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class Vet extends Person {
+    private Set<Specialty> specialties = new HashSet<>();
 }
