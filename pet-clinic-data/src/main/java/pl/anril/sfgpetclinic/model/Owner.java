5d15226b51cceab85af5b4ea6681cd6518bdd622
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-01-10 21:40:13 +0100

    adding Visit entity, closes #25

--- pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Owner.java
@@ -1,4 +1,11 @@
 package pl.anril.sfgpetclinic.model;
 
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
 public class Owner extends Person {
+    private Set<Pet> pets = new HashSet<>();
 }
