5408547e0c09e280b24c40cefd245502858e0241
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-01-31 21:42:13 +0100

    base entity jpa, closes #34

--- pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/BaseEntity.java
@@ -2,9 +2,16 @@ package pl.anril.sfgpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Data
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
