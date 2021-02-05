771e1d3e56b0156c5c287dd014ed30e8a88816e9
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-02-05 20:55:58 +0100

    add jpa to owners and other pojos, closes #35

--- pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Owner.java
@@ -3,14 +3,21 @@ package pl.anril.sfgpetclinic.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
     @EqualsAndHashCode.Exclude
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 }
--- pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/anril/sfgpetclinic/model/Pet.java
@@ -2,12 +2,21 @@ package pl.anril.sfgpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
-    private Owner owner;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
 }
